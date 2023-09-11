955bc0c870d79fd63bb5b4db1c8a01dba862e037
MormoManturtle <2310004@cellegebart.ca>
2023-09-11 09:33:30 -0400

    pull before day starts

--- Assets/scripts/PartyManager.cs
+++ Assets/scripts/PartyManager.cs
@@ -1,9 +1,11 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum EPartyMember
 {
@@ -14,35 +16,37 @@ public enum EPartyMember
     Rifleman
 }
 
-public class Abilities : ScriptableObject
-{
-
-}
-
-
-
-[CreateAssetMenu]
-public class PartyMember : ScriptableObject
-{
-    [SerializeField]int maxHP;
-    [SerializeField] int maxAP;
-    [SerializeField] int physDMG;
-    [SerializeField] int magDMG;
-
-    [SerializeField]GameObject prefab;
-    Abilities[] abilities;
-}
-
 
 public class PartyManager : MonoBehaviour
 {
-    [SerializeField]private List<EPartyMember> activeParty;
+    [SerializeField]private List<PartyMembers> activeParty;
+    [SerializeField] private List<PartyMembers> AvailableChars;
+
+    [SerializeField] private UnityEngine.UI.Image[] Selected;
+    [SerializeField] private Sprite[] images;
 
 
     public void AddToParty(int memberInt)
     {
-        var enums = (EPartyMember[])Enum.GetValues(typeof(EPartyMember));
+        if (activeParty.Count < 3) 
+        {
+            //var enums = (EPartyMember[])Enum.GetValues(typeof(EPartyMember));
 
-        activeParty.Add(enums[memberInt]);
+            activeParty.Add(AvailableChars[memberInt]);
+
+            for (int i = 0; i<=3; i++) 
+            {
+                if (Selected[i].sprite == null)
+                {
+                    Selected[i].sprite = activeParty[i].Portrait;
+                    break;
+                }
+            }
+
+            //return;
+        }
+
+
+
     }
 }
---
+++ Assets/scripts/PartyMembers.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class PartyMembers : ScriptableObject
+{
+    [SerializeField] public EPartyMember memberName;
+    [SerializeField] public int maxHP;
+    [SerializeField] public int maxAP;
+    [SerializeField] public int physDMG;
+    [SerializeField] public int magDMG;
+    [SerializeField] public Sprite Portrait;
+
+    [SerializeField] GameObject prefab;
+    Abilities[] abilities;
+
+}
+
+
+public class Abilities : ScriptableObject
+{
+
+}
