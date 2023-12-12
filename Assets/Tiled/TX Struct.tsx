bf00f29e1e3b2624bff85c0282cc4fe5139a1854
Alex Pelley <apelley98@gmail.com>
2023-12-11 19:37:48 -0500

    merge

--- Assets/Tiled/TX Struct.tsx
+++ Assets/Tiled/TX Struct.tsx
@@ -1,4 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="TX Struct" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.10" tiledversion="1.10.1" name="TX Struct" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="Cainos/Pixel Art Top Down - Basic/Texture/TX Struct.png" width="512" height="512"/>
+ <tile id="77">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32.3636,19.2727 21.8182,18.9091 15.4545,27.0909 15.4545,32 0,32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="78">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0.181818,18.9091 10,19.0909 16.1818,24.9091 16.1818,32.5455 32,32 32,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="93">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="17" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="94">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15" y="0" width="17" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
